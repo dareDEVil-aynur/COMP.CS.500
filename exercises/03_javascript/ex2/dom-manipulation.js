e9893ad9d631183c57476aede1ac678b6c39ccca
Anh Pham <anh.pham@tuni.fi>
2024-09-10 16:09:51 +0300

    Update round 03 upstream

--- exercises/03_javascript/ex2/dom-manipulation.js
+++ exercises/03_javascript/ex2/dom-manipulation.js
@@ -1,40 +1,29 @@
 /**
-* Sort table rows alphabetically based on the values in a column
-*
-* @param {Number} col column index (zero based)
-* @param {HTMLTableElement} table the table to be sorted
-*/
+ * Sort table rows alphabetically based on the values in a column
+ *
+ * @param {Number} col column index (zero based)
+ * @param {HTMLTableElement} table the table to be sorted
+ */
 function sortTableByColumn(col, table) {
-    // TODO: Implement this function as instructed
-    const tbody = table.querySelector('tbody');
-    const rows = Array.from(tbody.getElementsByTagName('tr'));
-
-    rows.sort((a, b) => {
-        const textA = a.cells[col].textContent.trim().toLowerCase();
-        const textB = b.cells[col].textContent.trim().toLowerCase();
-        return textA.localeCompare(textB);
-    });
-
-    rows.forEach(row => tbody.appendChild(row));
-
+  // TODO: Implement this function as instructed
 }
 
 /**
-* DO NOT EDIT THE CODE BELOW!
-*
-* The code below is there just to make it easier to test the code.
-*
-* If your function works correctly you should be able to sort the table
-* simply by clicking any column heading and the table should be immediately
-* sorted by values in that column.
-*/
+ * DO NOT EDIT THE CODE BELOW!
+ *
+ * The code below is there just to make it easier to test the code.
+ *
+ * If your function works correctly you should be able to sort the table
+ * simply by clicking any column heading and the table should be immediately
+ * sorted by values in that column.
+ */
 
 // find the table element
 const table = document.getElementById('sortable');
 
 // attach an event listener to each th element's click event
 table.querySelectorAll('thead th').forEach((th, i) =>
-    th.addEventListener('click', () => {
-        sortTableByColumn(i, table);
-    })
+  th.addEventListener('click', () => {
+    sortTableByColumn(i, table);
+  })
 );
