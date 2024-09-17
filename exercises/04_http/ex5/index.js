a6ce2f5596c653c3f542a06c6b78acdb99847e10
Aynur Rahman Talukdar <128217234+dareDEVil-aynur@users.noreply.github.com>
2024-09-17 15:41:03 +0300

    04_ex5

--- exercises/04_http/ex5/index.js
+++ exercises/04_http/ex5/index.js
@@ -3,49 +3,60 @@ const fs = require('fs');
 const path = require('path');
 
 http.createServer(function (request, response) {
+  const accHeader = request.headers['accept'];
 
-  const accHeader= request.headers['accept'];
-  
-  //TODO: implement sending responds to requests for XML, TXT, JSON, and */* handling JSON provided as an example.
-  if ( (request.headers['accept'].includes('application/json')) || (request.headers['accept'].includes('*/*'))) {
-    // You can use the readFileSendResponse(fileName, contentType, response) function 
-    // to read a file and send the response or write your own. It's up to you. :-)
-    readFileSendResponse('data.json' , 'application/json', response);
+  // Handle JSON
+  if (accHeader.includes('application/json')) {
+    readFileSendResponse('data.json', 'application/json', response);
+  } 
+  // Handle XML
+  else if (accHeader.includes('application/xml')) {
+    readFileSendResponse('data.xml', 'application/xml', response);
+  } 
+  // Handle TXT
+  else if (accHeader.includes('text/plain')) {
+    readFileSendResponse('data.txt', 'text/plain', response);
   }
-  else if (request.headers['accept'].includes('application/xml')) {
-    // You can use the readFileSendResponse(fileName, contentType, response) function 
-    // to read a file and send the response or write your own. It's up to you. :-)
-    readFileSendResponse('data.xml' , 'application/xml', response);
+  // Handle CSS
+  else if (accHeader.includes('text/css')) {
+    readFileSendResponse('data.css', 'text/css', response);
   }
-
-  else if (request.headers['accept'].includes('text/plain')) {
-    // You can use the readFileSendResponse(fileName, contentType, response) function 
-    // to read a file and send the response or write your own. It's up to you. :-)
-    readFileSendResponse('data.txt' , 'text/plain', response);
+  // Handle HTML
+  else if (accHeader.includes('text/html')) {
+    readFileSendResponse('data.html', 'text/html', response);
   }
-
-
+  // Handle ZIP
+  else if (accHeader.includes('application/zip')) {
+    readFileSendResponse('data.zip', 'application/zip', response);
+  }
+  // Handle */* (any type is acceptable, defaults to text/plain)
+  else if (accHeader.includes('*/*')) {
+    readFileSendResponse('data.txt', 'text/plain', response);
+  } 
+  // If none of the accepted content types match, return 406 error
   else {
     response.statusCode = 406;
     response.statusMessage = 'Content type not available';
     response.end();
   }
-}).listen(3000);
+}).listen(3000, () => {
+  console.log('Server running on port 3000');
+});
 
-/* 
-  * @param {string} fileName - name of the file to be read
-  * @param {string} contentType - type of the content to be sent in the response
-  * @param {object} response - response object
-  */
+/**
+ * @param {string} fileName - name of the file to be read
+ * @param {string} contentType - type of the content to be sent in the response
+ * @param {object} response - response object
+ */
 const readFileSendResponse = (fileName, contentType, response) => {
   fs.readFile(path.resolve(fileName), function (error, file) {
     if (error) {
-      response.writeHead(404);
-      response.write('An error occured: ', error);
+      response.writeHead(404, { 'Content-Type': 'text/plain' });
+      response.write('File not found');
     } else {
       response.writeHead(200, { 'Content-Type': contentType });
       response.write(file);
     }
     response.end();
-  })
-}
+  });
+};
