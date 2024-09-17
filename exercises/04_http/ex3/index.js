83f1cf1199612dd8d6e5586e73b86235cb1b14f7
Aynur Rahman Talukdar <128217234+dareDEVil-aynur@users.noreply.github.com>
2024-09-17 14:44:52 +0300

    fix 4

--- exercises/04_http/ex3/index.js
+++ exercises/04_http/ex3/index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer(function(request, response) {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.writeHead(200, { 'Content-Type': 'text/html' });
     
     const headers = request.headers;
 
