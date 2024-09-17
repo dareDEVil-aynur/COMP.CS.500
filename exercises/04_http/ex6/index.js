2758ca7773934e07bfb028d3086ca12ef8bf0177
Aynur Rahman Talukdar <128217234+dareDEVil-aynur@users.noreply.github.com>
2024-09-17 15:45:33 +0300

    04_ex6

--- exercises/04_http/ex6/index.js
+++ exercises/04_http/ex6/index.js
@@ -4,9 +4,9 @@ const path = require('path');
 
 http
   .createServer(function (request, response) {
-    if (request.url === '/homer') {
+    if (request.url === '/classical') {
       readFileSendResponse('homer.html', 'text/html', response);
-    } else if (request.url === '/bradbury') {
+    } else if (request.url === '/dystopy') {
       readFileSendResponse('bradbury.html', 'text/html', response);
     } else if (request.url === '/') {
       readFileSendResponse('index.html', 'text/html', response);
@@ -16,18 +16,19 @@ http
       response.end();
     }
   })
-  .listen(3000);
+  .listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
 
 function readFileSendResponse(fileName, contentType, response) {
-  fs.readFile(path.resolve(fileName), function (error, file) {
+  fs.readFile(path.resolve(__dirname, fileName), function (error, file) {
     if (error) {
-      response.writeHead('404');
-      response.write('An error occured: ', error);
+      response.writeHead(404, { 'Content-Type': 'text/plain' });
+      response.write('File not found');
     } else {
       response.writeHead(200, { 'Content-Type': contentType });
       response.write(file);
     }
-
     response.end();
   });
 }
