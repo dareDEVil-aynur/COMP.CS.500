6c1a3c3f24b86963a5e85973a17bff879a974b70
Aynur Rahman Talukdar <128217234+dareDEVil-aynur@users.noreply.github.com>
2024-09-24 14:56:47 +0300

    05_ex5

--- exercises/05_cookie/ex5/server.js
+++ exercises/05_cookie/ex5/server.js
@@ -1,6 +1,6 @@
 // Import the Express module
 const express = require('express');
-const app = express();  // Create an Express application
+const app = express(); // Create an Express application
 
 // TODO: Create a route that responds with "Welcome to my simple Express.js server!" when the user visits "/"
 app.get('/', (req, res) => {
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 
 // TODO: Create a route that responds with "This is the about page." when the user visits "/about"
 app.get('/about', (req, res) => {
-  
+  res.send('This is the about page.');
 });
 
 // TODO: Create a route that responds with JSON data for email and phone when the user visits "/contact"
@@ -19,20 +19,30 @@ app.get('/about', (req, res) => {
 //   phone: '123-456-7890'
 // }
 app.get('/contact', (req, res) => {
-
+  res.send({
+    email: 'contact@myserver.com',
+    phone: '123-456-7890',
+  });
 });
 
-// TODO: Create a route that responds with "Hello, username! You are age years old." when the user visits /user/:username with a query parameter age. 
+// TODO: Create a route that responds with "Hello, username! You are age years old." when the user visits /user/:username with a query parameter age.
 // If such query parameter is not provided, the route should respond with "Hello, username! Age not provided."
 app.get('/user/:username', (req, res) => {
+  const username = req.params.username;
+  const age = req.query.age;
 
+  if (age) {
+    res.send(`Hello, ${username}! You are ${age} years old.`);
+  } else {
+    res.send(`Hello, ${username}! Age not provided.`);
+  }
 });
 
 // DO NOT MODIFY BELOW THIS LINE
 // Conditionally start the server if this script is run directly
 if (require.main === module) {
   app.listen(3000, () => {
-      console.log('Server is running on http://localhost:3000');
+    console.log('Server is running on http://localhost:3000');
   });
 }
 
