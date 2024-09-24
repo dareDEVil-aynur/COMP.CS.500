212308916ac5b7e87c7205389c749b9554509cd4
Aynur Rahman Talukdar <128217234+dareDEVil-aynur@users.noreply.github.com>
2024-09-24 14:23:58 +0300

    05_ex2

--- exercises/05_cookie/ex2/script.js
+++ exercises/05_cookie/ex2/script.js
@@ -2,4 +2,11 @@ function setCookies() {
   // TODO: Get form values. getElementById() might be useful here.
 
   // TODO: Set cookie for each form value.
+  const text1 = Document.getElementById("text1")
+  const text2 = Document.getElementById("text2")
+  const checkbox = Document.getElementById("checkbox")
+
+  document.cookie = `text1=${text1}`
+  document.cookie = `text2=${text2}`
+  document.checkbox = `checkbox=${checkbox}`
 }
