f012122945e85ebaf0f33584f0e6f6663dd2a519
Aynur Rahman Talukdar <128217234+dareDEVil-aynur@users.noreply.github.com>
2024-10-09 14:11:09 +0300

    07_ex3

--- exercises/07_security/ex3/index.js
+++ exercises/07_security/ex3/index.js
@@ -10,25 +10,23 @@ const server = http.createServer((request, response) => {
     response.end();
   });
 
-
   if (request.url === '/' || request.url === undefined) {
-    fs.readFile(path.resolve('index.html'), function(error, file) {
+    fs.readFile(path.resolve('index.html'), function (error, file) {
       if (error) {
         response.writeHead(404);
         response.end(JSON.stringify(error));
         return;
       } else {
-        // TODO: modify Content Security 
-        // Policy header value as required
         response.writeHead(200, {
-          'Content-Security-Policy': 'default-src \'none\''
+          'Content-Security-Policy':
+            ' img-src www.flickr.com *.www.flickr.com staticflickr.com *.staticflickr.com ; script-src "self" localhost',
         });
         response.write(file);
       }
       response.end();
-    })
+    });
   } else if (request.url === '/js/let_me_run.js') {
-    fs.readFile(path.resolve('js/let_me_run.js'), function(error, file) {
+    fs.readFile(path.resolve('js/let_me_run.js'), function (error, file) {
       if (error) {
         response.writeHead(404);
         response.end(JSON.stringify(error));
@@ -38,7 +36,7 @@ const server = http.createServer((request, response) => {
         response.write(file);
       }
       response.end();
-    })
+    });
   } else {
     response.statusCode = 404;
     response.statusMessage = 'Requested content not found';
