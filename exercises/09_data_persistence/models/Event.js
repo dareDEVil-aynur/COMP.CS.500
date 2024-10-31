e376786deadb102edc20ec5c9d563a6ebe8fc1c2
Aynur Rahman Talukdar <128217234+dareDEVil-aynur@users.noreply.github.com>
2024-10-31 09:45:16 +0200

    event model

--- exercises/09_data_persistence/models/Event.js
+++ exercises/09_data_persistence/models/Event.js
@@ -1,7 +1,29 @@
 const mongoose = require('mongoose');
 
 const eventSchema = new mongoose.Schema({
-    //TODO: Fill in the schema
+  name: {
+    type: String,
+    required: [true, 'Name is required!'],
+    minlength: 1,
+    maxlength: 50,
+  },
+  date: {
+    type: String,
+    required: [true, 'Date is required!'],
+  },
+  status: {
+    type: String,
+    enum: {
+      values: ['planned', 'completed', 'cancelled', 'rejected'],
+      message:
+        'Status must be either planned, completed, cancelled, or rejected!',
+    },
+    default: 'planned',
+  },
+  description: {
+    type: String,
+    required: false,
+  },
 });
 
 const Event = mongoose.model('Event', eventSchema);
